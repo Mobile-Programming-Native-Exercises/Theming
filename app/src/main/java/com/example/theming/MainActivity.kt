4b6ab36f3636853f77351d3cd637b056debb5a3a
PortalFan202 <anibaldonoso1@gmail.com>
2023-01-30 13:23:25 +0200

    Completed assignment

--- app/src/main/java/com/example/theming/MainActivity.kt
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.theming
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.theming.ui.theme.ThemingTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ThemingTheme {
                 // A surface container using the 'background' color from the theme
-
+                MyApp()
             }
         }
     }
@@ -25,10 +30,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    Column {
-        Text(text = "Title")
-        OutlinedTextField(value = "", onValueChange = {/*TODO*/})
-        Button(onClick = { /*TODO*/ }) {
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(
+            text = "Title",
+            style = MaterialTheme.typography.h5,
+            modifier = appModifier
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {/*TODO*/ },
+            modifier = appModifier
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = appModifier
+        ) {
             Text(text = "Submit")
         }
     }
