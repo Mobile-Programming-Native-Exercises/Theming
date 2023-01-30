5b470a170409fcc3094d4bf610fc7d0c716ceace
PortalFan202 <anibaldonoso1@gmail.com>
2023-01-30 13:10:34 +0200

    Created Base UI

--- app/src/main/java/com/example/theming/MainActivity.kt
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.theming
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,26 +17,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             ThemingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ThemingTheme {
-        Greeting("Android")
+fun MyApp() {
+    Column {
+        Text(text = "Title")
+        OutlinedTextField(value = "", onValueChange = {/*TODO*/})
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Submit")
+        }
     }
 }
